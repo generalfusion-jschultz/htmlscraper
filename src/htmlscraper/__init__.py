00ef0421e1c18b39d17dd74e9e52a34abd154fa3
Jason Schultz <jason.schultz@generalfusion.com>
2024-01-17 15:07:51 -0800

    Updated imports

--- src/htmlscraper/__init__.py
+++ src/htmlscraper/__init__.py
@@ -1,7 +1,4 @@
 __version__ = "0.1.0"
 
-from htmlscraper.htmlscraper import ScrapedData
-from htmlscraper.htmlscraper import extract_timestamp_from_strings
-from htmlscraper.htmlscraper import scrape_timestamp_from_soup
-from htmlscraper.htmlscraper import display_data
-from htmlscraper.htmlscraper import scrape_data
+from htmlscraper.htmlscraper import Scraper
+from htmlscraper.htmlscraper import ScraperTextFile
